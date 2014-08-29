6b27f1947a0519de2ac0c62be997a506375149b3
KO Myung-Hun <komh@chollian.net>
2014-08-29 12:51:47 +0900

    typedef socklen_t
        new file:   network/socklen_t.h

---
+++ network/socklen_t.h
@@ -0,0 +1,18 @@
+/*
+ * typedef socklen_t for OS/2 kLIBC
+ *
+ * Copyright (C) 2014 KO Myung-Hun <komh@chollian.net>
+ *
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ */
+
+#ifndef OS2COMPAT_SOCKLEN_T_H
+#define OS2COMPAT_SOCKLEN_T_H
+
+typedef int socklen_t;
+
+#endif
