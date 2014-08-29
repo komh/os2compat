98bdb8f609241506579a80ac84625359b6bb8dbd
KO Myung-Hun <komh@chollian.net>
2014-08-29 12:52:21 +0900

    Define macros for shutdown()
        new file:   network/shutdown.h

---
+++ network/shutdown.h
@@ -0,0 +1,28 @@
+/*
+ * Define macros for shutdown() for OS/2 kLIBC
+ *
+ * Copyright (C) 2014 KO Myung-Hun <komh@chollian.net>
+ *
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ */
+
+#ifndef OS2COMPAT_SHUTDOWN_H
+#define OS2COMPAT_SHUTDOWN_H
+
+#ifndef SHUT_RD
+#define SHUT_RD 0
+#endif
+
+#ifndef SHUT_WR
+#define SHUT_WR 1
+#endif
+
+#ifndef SHUT_RDWR
+#define SHUT_RDWR 2
+#endif
+
+#endif
